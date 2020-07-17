1535c291e9f1047fed552d16ac9cc61d462841a6
Ethan Jon <ethanczahor@gmail.com>
2020-07-17 04:10:15 -0700

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import React from 'react'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <div style={{ backgroundColor: 'purple' }}>this is the home page</div>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      marbleImage: file(relativePath: { eq: "marble.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 2400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <div className='flex items-center justify-center relative overflow-hidden' style={{ height: '40vh' }}>
+        <div className='absolute w-full'>
+          <Img fluid={data.marbleImage.childImageSharp.fluid} />
+        </div>
+
+        <div className='relative text-white text-center' style={{ maxWidth: 716 }}>
+          <h2>A next generation constituent experience management platform designed for government</h2>
+          <div>
+            <button className='uppercase text-governor-bay' style={{ backgroundColor: '#E0E0E0' }}>
+              Request a demo
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
