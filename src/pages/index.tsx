9a679d07ed61503bcf9a9031bf3e9f2207267e31
Ethan Jon <ethanczahor@gmail.com>
2020-07-18 03:04:27 -0700

    polishing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 
 import Accordion from '../components/accordion'
 import Layout from '../components/layout'
+// @ts-ignore
+import Clock from '../images/clock.svg'
 
 // @ts-ignore
 import OpenTableLogo from '../images/logo-opentable.svg'
@@ -70,21 +72,31 @@ const IndexPage = () => {
         </div>
       </div>
 
-      <div className='md:grid grid-cols-2'>
+      <div className='md:grid grid-cols-2 py-12'>
         <div style={{ maxWidth: 500, justifySelf: 'end' }}>
-          <h2>Reduce your response time from weeks to hours</h2>
-          <div>
+          <h2 className='tiempos-headline text-4xl leading-tight mb-6'>Reduce your response time from weeks to hours</h2>
+          <div className='text-lg'>
             Over the past 15 years government officials have seen an explosion in volume of incoming requests. New digital channels, new expectations and a changing relationship
             with those they serve has made it difficult to keep pace. Indigov was built to alleviate this increasing burden and help our democracy scale better.
           </div>
         </div>
 
         <div>
-          <div>AVERAGE RESPONSE TIME FOR A  MEMBER OF US CONGRESS TO A CONSTITUENT MESSAGE</div>
-          <div>32-64 days</div>
-          <div>Legacy systems</div>
-          <div>8 hours</div>
-          <div>Indigov</div>
+          <hr />
+          <div className='flex my-4 items-center'>
+            <div className='montserrat uppercase font-medium tracking-wider leading-snug mr-6' style={{ color: '#6D7278', width: 300 }}>
+              AVERAGE RESPONSE TIME FOR A MEMBER OF US CONGRESS TO A CONSTITUENT MESSAGE
+            </div>
+            <div>
+              <Clock />
+            </div>
+          </div>
+          <div className='mb-4'>
+            <div className='tiempos-headline text-5xl text-portage'>32-64 days</div>
+            <div className='montserrat uppercase tracking-widest text-sm'>Legacy systems</div>
+          </div>
+          <div className='tiempos-headline text-5xl text-governor-bay'>8 hours</div>
+          <div className='montserrat uppercase tracking-widest text-sm'>Indigov</div>
         </div>
       </div>
 
