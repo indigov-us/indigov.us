04aed01ca8ceda8137a173a545ef4fcfba662953
Ethan Jon <ethanczahor@gmail.com>
2020-07-19 17:30:54 -0700

    quote section

--- src/components/row-d.tsx
+++ src/components/row-d.tsx
@@ -72,12 +72,12 @@ export default () => {
   const onCarouselChange = (value) => setCarouselValue(value)
 
   return (
-    <div className='md:grid grid-cols-3' style={{ background: 'linear-gradient(180deg, #F8F8F8 0%, #FFFFFF 100%)' }}>
-      <div className='self-end hidden md:block pt-20'>
+    <div className='md:grid grid-cols-8' style={{ background: 'linear-gradient(180deg, #F8F8F8 0%, #FFFFFF 100%)' }}>
+      <div className='self-end hidden md:block pt-20 col-span-2'>
         <Img fluid={data.bennyboyImage.childImageSharp.fluid} style={{ maxWidth: 357 }} />
       </div>
 
-      <div className='text-center self-center py-10 mx-6 col-span-2 lg:col-span-1'>
+      <div className='text-center self-center py-10 mx-6 col-span-6 md:px-10 lg:px-20'>
         <div className='montserrat uppercase tracking-widest text-xl'>What staffers are saying</div>
 
         <Carousel className='mb-10' plugins={['infinite']} value={carouselValue} onChange={onCarouselChange}>
