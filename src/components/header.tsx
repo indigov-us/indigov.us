567515df2719a10e9c69741a9422cf201ea4eb2f
Ethan Jon <ethanczahor@gmail.com>
2020-07-20 05:26:46 -0700

    header polishes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,15 @@ import Logo from '../images/logo.svg'
 export const requestADemoHref = 'mailto:contact@indigov.us'
 
 const Header = () => (
-  <header className='sticky top-0 py-10 bg-white w-full'>
+  <header className='sticky top-0 py-8 bg-white w-full z-10'>
     <div className='container'>
-      <div className='flex justify-center md:grid md:grid-cols-2'>
-        <div>
-          <Link to='/'>
-            <Logo />
-          </Link>
-        </div>
+      <div className='flex flex-col md:flex-row items-center justify-between'>
+        <Link to='/'>
+          <Logo />
+        </Link>
 
-        <div className='text-right hidden md:block'>
-          <a className='px-5 py-3 tracking-widest text-xs bg-governor-bay text-white uppercase font-bold' href={requestADemoHref} target='_blank'>
+        <div className='mt-4 md:mt-0 hidden md:block'>
+          <a className='inline-block px-5 py-3 tracking-widest text-xs bg-governor-bay text-white uppercase font-bold' href={requestADemoHref} target='_blank'>
             Request a demo
           </a>
         </div>
