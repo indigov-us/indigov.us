73a4e056c78e6ba717656ce42c733d4904a8589e
Ben Jacobsno <ben.jacobson@indigov.us>
2021-02-11 10:14:45 -0700

    Add placeholder support page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <div style={{textAlign:'center'}}>
+            <h1 style={{fontSize: '1.5em', fontWeight: 'bold', margin: '10px 0 20px 0'}}>404 — NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage
