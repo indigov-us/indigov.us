ec5f0d4e7bc595e94da4c755ff17e43496594f20
Ethan Jon <ethanczahor@gmail.com>
2021-10-01 13:25:22 -0500

    showing menu on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,14 +8,14 @@ export const requestADemoHref = 'mailto:contact@indigov.com'
 export const careersHref = 'https://jobs.lever.co/indigov'
 
 const Header = () => (
-  <header className='sticky top-0 py-8 bg-white w-full z-10'>
+  <header className='sticky top-0 pt-6 pb-4 md:pb-6 bg-white w-full z-10'>
     <div className='container'>
       <div className='flex flex-col md:flex-row items-center justify-between'>
         <Link to='/'>
           <Logo />
         </Link>
 
-        <div className='mt-4 md:mt-0 hidden md:flex items-center -mx-6'>
+        <div className='mt-6 md:mt-0 flex items-center -mx-6'>
           <div className='px-6'>
             <Link className='montserrat uppercase tracking-widest text-sm' to='/#products' activeClassName='font-bold border-b-2'>
               Products
@@ -34,7 +34,7 @@ const Header = () => (
             </a>
           </div>
 
-          <div className='px-6'>
+          <div className='px-6 hidden lg:block'>
             <a className='inline-block px-5 py-3 tracking-widest text-xs bg-governor-bay text-white uppercase font-bold' href={requestADemoHref} target='_blank'>
               Request a demo
             </a>
